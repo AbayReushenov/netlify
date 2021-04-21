8453a9dcc390c492379bb5c32eab5f144780fefa
Abay <abay.reushenov@gmail.com>
2021-04-21 08:26:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const path = require('path');
+const express = require('express');
+
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static(path.join(__dirname, '.', 'public')));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '.', 'public', 'index.html'));
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log('server started on port 5000');
+});
